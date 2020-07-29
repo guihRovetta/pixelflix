b85f64d4a3c73de9c8a6a0a2dfd35573dd2d19b0
guihRovetta <guilherme020213@hotmail.com>
2020-07-29 12:30:31 -0300

    added form-field component

--- src/pages/Registration/Category/index.js
+++ src/pages/Registration/Category/index.js
@@ -1,14 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+
+import { Container, Title } from './styles';
 
 function Category() {
+  const initialValues = { name: '', description: '', color: '#FFFFFF' };
+  const [values, setValues] = useState(initialValues);
+  const [categories, setCategories] = useState([]);
+
+  function setValue(key, value) {
+    setValues({
+      ...values,
+      [key]: value,
+    });
+  }
+
+  function handleChange(event) {
+    setValue(event.target.getAttribute('name'), event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setCategories([...categories, values]);
+    setValues(initialValues);
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <Container>
+        <Title>Nova Categoria</Title>
 
-      <Link to="/registration/video">Cadastrar vídeo</Link>
+        <form onSubmit={handleSubmit}>
+          <FormField
+            label="Nome da Categoria"
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+          />
+          <FormField
+            label="Descrição"
+            type="text"
+            name="description"
+            value={values.description}
+            onChange={handleChange}
+          />
+          <FormField
+            label="Cor"
+            type="color"
+            name="color"
+            value={values.color}
+            onChange={handleChange}
+          />
+
+          <button>Salvar</button>
+        </form>
+        <div>{categories.length}</div>
+        <ul>
+          {categories.map((category, index) => {
+            return <li key={`${category}${index}`}>{category.name}</li>;
+          })}
+        </ul>
+        <Link to="/registration/video">Cadastrar vídeo</Link>
+      </Container>
     </PageDefault>
   );
 }
---
+++ src/pages/Registration/Category/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  font-size: 60px;
+  color: var(--grayLight);
+  margin-bottom: 91px;
+`;
